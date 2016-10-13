ca77636546ca67910853e47220d723c1f06258f9
Tomi Lamsa <lamsatom@gmail.com>
2016-10-13 13:29:57 +0300

    updated R script, removed rows with failures, script ot make combined bar graph of execution times

--- notes_scripts.R
+++ notes_scripts.R
@@ -2,25 +2,47 @@ setwd("C:/Users/Tomi/testAutomation/measurements/combined_results/notes")
 library(ggplot2)
 library(gridExtra)
 
-#munch munch
+#munch munch, clean rows with failures from the results
 notes_appium <- read.csv("appium_notes.csv")
+notes_appium <- notes_appium[!(notes_appium$failures != 0),]
 notes_appium$toolname <- "Appium"
 
 notes_espresso <- read.csv("espresso_notes.csv")
+notes_espresso <- notes_espresso[!(notes_espresso$failures != 0),]
 notes_espresso$toolname <- "Espresso"
 
 notes_robotium <- read.csv("robotium_notes.csv")
+notes_robotium <- notes_robotium[!(notes_robotium$failures != 0),]
 notes_robotium$toolname <- "Robotium"
 
 notes_uiautomator <- read.csv("uiautomator_notes.csv")
+notes_uiautomator <- notes_uiautomator[!(notes_uiautomator$failures != 0),]
 notes_uiautomator$toolname <- "UiAutomator"
 
 notes_tau <- read.csv("tau_notes.csv")
+notes_tau <- notes_tau[!(notes_tau$failures != 0),]
 notes_tau$toolname <- "Tau"
 
 #data together
 notes_combined <- rbind(notes_appium, notes_espresso, 
                         notes_robotium, notes_uiautomator, notes_tau)
+
+
+#bar plots of the individual results
+appium_runtime <- qplot(x = runTime_seconds, data = subset(notes_appium, failures == 0),
+                        binwidth = 1, xlab = "Appium") + 
+  scale_x_continuous(limits = c(72, 560))
+espresso_runtime <- qplot(x = runTime_seconds, data = notes_espresso, binwidth = 1, xlab = "Espresso") +
+  scale_x_continuous(limits = c(72, 560))
+robotium_runtime <- qplot(x = runTime_seconds, data = subset(notes_robotium, failures == 0),
+                          binwidth = 1, xlab = "Robotium") +
+  scale_x_continuous(limits = c(72, 560))
+tau_runtime <- qplot(x = runTime_seconds, data = notes_tau, binwidth = 1, xlab = "Tau") +
+  scale_x_continuous(limits = c(72, 560))
+uiautomator_runtime <- qplot(x = runTime_seconds, data = notes_uiautomator, binwidth = 1, xlab = "uiautomator") +
+  scale_x_continuous(limits = c(72, 560))
+
+grid.arrange(appium_runtime, espresso_runtime, robotium_runtime, tau_runtime, uiautomator_runtime, ncol = 1)
 
 #boxplot from all the tools
 qplot(x = toolname, y = runTime_seconds, data = notes_combined, geom = "boxplot",
@@ -35,11 +57,6 @@ ggplot(notes_espresso, aes(x = runTime_seconds)) + geom_histogram()
 
 
 #means for the run times
-#koitetaan muokata numeroiksi muuttuja (voivat olla faktoreita), mutta tämä ei ilmeisesti auta
-#numeric_espresso <- mean(notes_espresso$runTime_seconds)
-#numeric_espresso <- as.numeric(as.character(numeric_espresso))
-#mean_espresso <- numeric_espresso
-#mean_espresso <- mean(as.numeric(levels(notes_espresso$runTime_seconds)))
 
 mean_espresso <- mean(notes_espresso$runTime_seconds)
 mean_espresso <- as.numeric(as.character(mean_espresso))
@@ -69,10 +86,10 @@ means = rbind(means, c("UiAutomator", mean_uiautomator))
 means = rbind(means, c("Tau", mean_tau))
 #convert time column to numeric
 means[, 2] <- as.data.frame(sapply(means[, 2], as.numeric))
-means$time <- as.numeric(as.character(means$time))
-means[,'time'] <- as.numeric(as.character(means[,'time']))
+#means$time <- as.numeric(as.character(means$time))
+#means[,'time'] <- as.numeric(as.character(means[,'time']))
 
-#histogrammi meaneista, jostain syystä espresso on väärässä paikassa
+#histogrammi meaneista
 ggplot(means, aes(x = toolname, y = time)) + geom_bar(stat = "identity")
 
 qplot(x = toolname, y = time, data = means) + geom_histogram(binwidth = 1)
@@ -85,7 +102,7 @@ install.packages("effsize", lib = "C:/Users/Tomi/R/gradu_r/effsize_0.6.4")
 library(effsize, lib.loc = "C:/Users/Tomi/R/gradu_r/effsize_0.6.4")
 #https://cran.r-project.org/web/packages/effsize/effsize.pdf
 
-appium_espresso <- rbind(notes_appium, notes_espresso)
+#appium_espresso <- rbind(notes_appium, notes_espresso)
 appium_espresso <- data.frame(notes_appium$toolname, notes_appium$runTime_seconds)
 #appium_espresso <- cbind(notes_espresso$toolname, notes_espresso$runTime_seconds)
 appium_espresso <- rbind(appium_espresso, notes_espresso[1,])
@@ -94,9 +111,21 @@ appium_espresso <- read.csv("appium_espresso.csv")
 
 #en tiiä mitä tällä yritetään, ei toimi
 cohen.d(d = mean(appium_espresso$runTime_seconds), f = appium_espresso$runTime_seconds)
-#cohenin d:n saa varmaan tällä:
+#cohenin d:n saa varmaan tällä, ei oikeesti:
 cohen.d(f = notes_espresso$runTime_seconds, d = notes_appium$runTime_seconds)
-#cohenin d mikan excelkaavion perusteella
-(mean(notes_appium$runTime_seconds) - mean(notes_espresso$runTime_seconds)) / sd(appium_espresso$runTime_seconds)
+
+#tehty kattomalla dokumentaation esimerkkiä
+d = (c(notes_appium$runTime_seconds, notes_espresso$runTime_seconds))
+f = rep(c("Appium","Espresso"), each=50)
+
+#calculate cohen's d
+cohen.d(notes_appium$runTime_seconds, notes_espresso$runTime_seconds)
+cohen.d(notes_tau$runTime_seconds, notes_robotium$runTime_seconds)
+cohen.d(notes_uiautomator$runTime_seconds, notes_tau$runTime_seconds)
+cohen.d(notes_robotium$runTime_seconds, notes_espresso$runTime_seconds)
+cohen.d(notes_tau$runTime_seconds, notes_robotium$runTime_seconds)
+#data and factor
+cohen.d(d,f)
+
 
 
